80e44e6659e48f14197a7709fa3149627f6b2edc
zbubster <zbu.rataj@seznam.cz>
2026-01-18 00:20:52 +0100

    added metadata check (CRS, origin, resolution,...)

--- scripts/merge_S2_rasters.R
+++ scripts/merge_S2_rasters.R
@@ -18,13 +18,25 @@ out_file <- here("data", "Sentinel2_MOSAIC.tif")
 files <- list.files(in_dir, pattern = "\\.tif$", full.names = TRUE)
 files <- sort(files)
 
+# check metadata
+check <- data.frame(
+  file   = basename(files),
+  nlyr   = sapply(files, \(f) nlyr(rast(f))),
+  res_x  = sapply(files, \(f) res(rast(f))[1]),
+  res_y  = sapply(files, \(f) res(rast(f))[2]),
+  orig_x = sapply(files, \(f) origin(rast(f))[1]),
+  orig_y = sapply(files, \(f) origin(rast(f))[2]),
+  crs    = sapply(files, \(f) crs(rast(f))),
+  stringsAsFactors = FALSE
+)
+
+sapply(check[, c("nlyr","res_x","res_y","orig_x","orig_y","crs")], function(x) length(unique(x)))
+# there should be always only 1 unique value:
+# nlyr  res_x  res_y orig_x orig_y   crs 
+#  1      1      1      1      1      1 
+
 # create SpatRasterCollection
 rc <- sprc(files)
-
-# little check
-stopifnot(length(rc) == 81)
-check <- rast(files[1])
-message("nlyr = ", nlyr(check), ", res = ", paste(res(check), collapse="x"))
 
 # GDAL write options
 wopt <- list(
@@ -37,7 +49,7 @@ wopt <- list(
 # Mosaic
 mosaic(
   rc,
-  fun = "first", # takes first layer value
+  fun = "first", # takes value of the first layer
   filename = out_file,
   overwrite = TRUE,
   wopt = wopt
