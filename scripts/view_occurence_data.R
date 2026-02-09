37e5d91ec77bd637d862601fad6b5d7742a22ff8
zbubster <zbu.rataj@seznam.cz>
2026-02-09 20:00:11 +0100

    remove old comments, add comments to new lines, other marginal changes

--- scripts/view_occurence_data.R
+++ scripts/view_occurence_data.R
@@ -10,59 +10,30 @@ library(maps)
 
 # Load data
 
+# data dir
 base_dir <- "data/occurence/__analysis__"
 
+# get filenames and species names (from filenames)
 files <- base::list.files(base_dir, pattern = "\\.gpkg$", full.names = TRUE)
 nm <- tools::file_path_sans_ext(base::basename(files))
 
+# get all sf layers
 all_sf <- purrr::set_names(files, nm) |>
   purrr::map(sf::st_read, quiet = TRUE)
 
-# presences / absences jako sf pointy (ponechávám species + geometrii)
-pres <- purrr::map(all_sf, \(x) dplyr::filter(x, .data$P_A == 1) |> dplyr::select(.data$species))
-abs  <- purrr::map(all_sf, \(x) dplyr::filter(x, .data$P_A == 0) |> dplyr::select(.data$species))
+# presences, absences (keep species & geometry)
+pres <- purrr::map(all_sf, \(x) dplyr::filter(x, .data$P_A == 1) |> dplyr::select(species))
+abs  <- purrr::map(all_sf, \(x) dplyr::filter(x, .data$P_A == 0) |> dplyr::select(species))
 
-# pořadí druhů (podle tvého původního seznamu)
+# focal plants
 wanted <- c(
-  "gen_tergestina","phy_orbiculare","pri_kitaibeliana","cam_velebitica",
-  "sax_blavii","cam_albanica","gen_utriculosa","gen_dinarica"
+  "gen_tergestina","phy_orbiculare",#"pri_kitaibeliana",
+  "cam_velebitica", "sax_blavii","cam_albanica","gen_utriculosa","gen_dinarica"
 )
 
+# subset lists to focal species
 speclist <- pres[wanted]
 abslist  <- abs[wanted]
-# 
-# # Occurence, extract only presence data
-# genter <- st_read("data/occurence/__analysis__/gen_tergestina.gpkg")
-# phytorb <- st_read("data/occurence/__analysis__/phy_orbiculare.gpkg")
-# prikit <- st_read("data/occurence/__analysis__/pri_kitaibeliana.gpkg")
-# camvel <- st_read("data/occurence/__analysis__/cam_velebitica.gpkg")
-# saxbla <- st_read("data/occurence/__analysis__/sax_blavii.gpkg")
-# camalb <- st_read("data/occurence/__analysis__/cam_albanica.gpkg")
-# genutri <- st_read("data/occurence/__analysis__/gen_utriculosa.gpkg")
-# gendin <- st_read("data/occurence/__analysis__/gen_dinarica.gpkg")
-# 
-# # Presences
-# genter_p <- genter[genter$P_A == 1, "species"]
-# phytorb_p <- phytorb[phytorb$P_A == 1, "species"]
-# prikit_p <- prikit[prikit$P_A == 1, "species"]
-# camvel_p <- camvel[camvel$P_A == 1, "species"]
-# saxbla_p <- saxbla[saxbla$P_A == 1, "species"]
-# camalb_p <- camalb[camalb$P_A == 1, "species"]
-# genutri_p <- genutri[genutri$P_A == 1, "species"]
-# gendin_p <- gendin[gendin$P_A == 1, "species"]
-# 
-# # Absences
-# genter_a <- genter[genter$P_A == 0, "species"]
-# phytorb_a <- phytorb[phytorb$P_A == 0, "species"]
-# prikit_a <- prikit[prikit$P_A == 0, "species"]
-# camvel_a <- camvel[camvel$P_A == 0, "species"]
-# saxbla_a <- saxbla[saxbla$P_A == 0, "species"]
-# camalb_a <- camalb[camalb$P_A == 0, "species"]
-# genutri_a <- genutri[genutri$P_A == 0, "species"]
-# gendin_a <- gendin[gendin$P_A == 0, "species"]
-# 
-# 
-# speclist <- list(genter_p, phytorb_p, prikit_p, camvel_p, saxbla_p, camalb_p, genutri_p, gendin_p)
 
 # Extent
 extent <- st_read("data/extent_raw.gpkg")
@@ -111,11 +82,9 @@ for (i in seq_along(speclist)) {
   terra::plot(m_3035_bound, add = TRUE, col = "grey30")
   terra::plot(sf::st_boundary(extent), add = TRUE, col = "firebrick", lwd = 4, lty = 3)
   
-  # abs, if any
-  has_abs <- !is.null(abslist[[i]]) && nrow(abslist[[i]]) > 0
-  if (has_abs) {
-    terra::plot(abslist[[i]], add = TRUE, pch = 16, col = "black", cex = 0.7)
-  }
+  # absences
+  terra::plot(abslist[[i]], add = TRUE, pch = 16, col = "black", cex = 0.7)
+    
   # presences
   terra::plot(speclist[[i]], add = TRUE, pch = 16, col = "green", cex = 1)
   
