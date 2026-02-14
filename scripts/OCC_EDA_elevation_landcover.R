de0e6ac733ce873aa07ea3d80437df4dbf72f880
zbubster <zbu.rataj@seznam.cz>
2026-02-14 20:29:20 +0100

    working on better LC script; OCC distribution visualisation

---
+++ scripts/OCC_EDA_elevation_landcover.R
@@ -0,0 +1,73 @@
+# - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
+# Occurence EDA, elevation
+# - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
+
+# Load data
+
+# data dir
+base_dir <- "data/occurence/__analysis__"
+
+# get filenames and species names (from filenames)
+files <- base::list.files(base_dir, pattern = "\\.gpkg$", full.names = TRUE)
+stopifnot(length(files) > 0)
+
+# načti všechny vrstvy
+lst <- lapply(files, function(f) sf::st_read(f))
+
+# CRS sjednoť podle první vrstvy
+crs_ref <- sf::st_crs(lst[[1]])
+lst <- lapply(lst, function(x) sf::st_transform(x, crs_ref))
+
+# spoj do jedné vrstvy
+pts_all <- dplyr::bind_rows(lst)
+
+pts_all
+
+pts_uniq <- pts_all %>%
+  dplyr::distinct(geom, .keep_all = TRUE)
+pts_uniq
+
+dem <- terra::rast("/media/zbub/DATA/DEM/DEM30_mosaic_cropped.tif")
+
+ex <- terra::extract(dem, pts_uniq)
+
+
+# vektory výšek
+z   <- ex$DEM
+
+# očista
+z   <- z[is.finite(z)]
+
+# histogram parametry (ať máme šířku binu)
+h <- graphics::hist(z, breaks = "FD", plot = FALSE)
+bin_w <- h$breaks[2] - h$breaks[1]
+
+# vykreslení histogramu (hlavní)
+graphics::hist(
+  z,
+  breaks = h$breaks,
+  main = paste0("Elevation of the observations\n", sprintf("(n = %d unique geometries)", length(z))),
+  xlab = "Elevation [m]",
+  ylab = "Freq",
+  border = "white"
+)
+
+# density (škálování na počty v histogramu)
+d_all <- stats::density(z)
+
+graphics::lines(d_all$x, d_all$y * length(z)   * bin_w, lwd = 3, col = "firebrick")
+
+graphics::legend(
+  "topright",
+  legend = "Density curve",
+  lwd = 3,
+  lty = 1,
+  col = "firebrick",
+  bty = "n"
+)
+
+
+x <- terra::rast("/media/zbub/DATA/terrascope_world_cover/WORLDCOVER/ESA_WC_4.tif")
+x
+hist(x)
+plot(x)
--- scripts/esa_wc.R
+++ scripts/esa_wc.R
@@ -28,6 +28,20 @@ files
 #   r
 # })
 
+# load SpatRasterCollection
+rc <- terra::sprc(files)
+
+r <- terra::mosaic(rc)
+extent <- vect("data/extent_raw.gpkg")
+aoi <- terra::project(extent, terra::crs(rc[1]))
+r_croped <- terra::crop(r, extent)
+r_masked <- terra::mask(r_croped, extent)
+r_final <- terra::project(r_masked, terra::crs(extent))
+
+
+
+
+
 for(i in seq_along(files)){
   print(paste0("R_", i, "_load"))
   r <- rast(files[i])
@@ -47,3 +61,38 @@ for(i in seq_along(files)){
 x <- rast("/media/zbub/DATA/terrascope_world_cover/WORLDCOVER/ESA_WC_5.tif")
 plot(x)
 x
+
+# merge
+
+wc_dir <- "/media/zbub/DATA/WORLDCOVER"
+
+files <- list.files(
+  path = wc_dir,
+  pattern = "\\.tif$",
+  full.names = TRUE
+)
+files
+
+# load SpatRasterCollection
+rc <- terra::sprc(files)
+
+# ref = první soubor jako šablona
+ref <- terra::rast(files[1])
+
+info <- do.call(
+  rbind,
+  lapply(files, function(f) {
+    r <- terra::rast(f)
+    data.frame(
+      file   = basename(f),
+      crs_ok = terra::same.crs(r, ref),
+      res_x  = terra::res(r)[1],
+      res_y  = terra::res(r)[2],
+      org_x  = terra::origin(r)[1],
+      org_y  = terra::origin(r)[2]
+    )
+  })
+)
+
+info
+
