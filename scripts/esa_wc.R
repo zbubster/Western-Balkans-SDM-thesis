6652a74ecaa0686d32b37790877631ec4f7ce52a
zbubster <zbu.rataj@seznam.cz>
2026-02-15 17:08:42 +0100

    data reloading

--- scripts/esa_wc.R
+++ scripts/esa_wc.R
@@ -1,7 +1,14 @@
-## ESA world cover
+# - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
+# ESA world cover
+# - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
 
-esa_dir <- "/media/zbub/DATA/terrascope_world_cover/WORLDCOVER"
-extent <- vect("data/extent_raw.gpkg")
+# - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
+
+# Load data
+
+# dirs & files
+esa_dir <- here("data", "WORLDCOVER")
+extent <- vect(here("data", "extent_raw.gpkg"))
 
 files <- list.files(
   path = esa_dir,
@@ -10,89 +17,51 @@ files <- list.files(
 )
 files
 
-# rs <- vector("list", length = length(files))
-# for(i in seq_along(files)){
-#   rs[[i]] <- rast(files[i])
-# }
-
-# x <- rast(files[2])
-# plot(x)
-# crs(x) == crs(extent)
-# x <- project(x, extent)
-
-# cuts <- lapply(files, function(f) {
-#   r <- rast(f)
-#   r <- project(r, extent)
-#   r <- crop(r, extent)
-#   r <- mask(r, extent)
-#   r
-# })
-
 # load SpatRasterCollection
 rc <- terra::sprc(files)
 
-r <- terra::mosaic(rc)
-extent <- vect("data/extent_raw.gpkg")
-aoi <- terra::project(extent, terra::crs(rc[1]))
-r_croped <- terra::crop(r, extent)
-r_masked <- terra::mask(r_croped, extent)
-r_final <- terra::project(r_masked, terra::crs(extent))
-
-
-
-
-
-for(i in seq_along(files)){
-  print(paste0("R_", i, "_load"))
-  r <- rast(files[i])
-  print(paste0("R_", i, "_project"))
-  r <- project(r, extent)
-  print(paste0("R_", i, "_crop"))
-  r <- crop(r, extent)
-  print(paste0("R_", i, "_mask"))
-  r <- mask(r, extent)
-  n <- paste0("ESA_WC_", i, ".tif")
-  print(paste0("R_", i, "_write"))
-  writeRaster(r, filename = paste0("/media/zbub/DATA/terrascope_world_cover/WORLDCOVER/", n))
-  print("GC")
-  rm(r, n); gc()
-}
-
-x <- rast("/media/zbub/DATA/terrascope_world_cover/WORLDCOVER/ESA_WC_5.tif")
-plot(x)
-x
-
-# merge
-
-wc_dir <- "/media/zbub/DATA/WORLDCOVER"
-
-files <- list.files(
-  path = wc_dir,
-  pattern = "\\.tif$",
-  full.names = TRUE
-)
-files
-
-# load SpatRasterCollection
-rc <- terra::sprc(files)
-
-# ref = první soubor jako šablona
-ref <- terra::rast(files[1])
-
-info <- do.call(
-  rbind,
-  lapply(files, function(f) {
-    r <- terra::rast(f)
-    data.frame(
-      file   = basename(f),
-      crs_ok = terra::same.crs(r, ref),
-      res_x  = terra::res(r)[1],
-      res_y  = terra::res(r)[2],
-      org_x  = terra::origin(r)[1],
-      org_y  = terra::origin(r)[2]
-    )
-  })
+# GDAL write options
+wopt <- list(
+  datatype = "FLT4S",
+  gdal = c("COMPRESS=LZW", "TILED=YES", "BIGTIFF=YES")
 )
 
-info
+# Mosaic
+mosaic(
+  rc,
+  fun = "first", # takes value of the first layer
+  filename = here("data", "WORLDCOVER", "1_ESA_WC_mosaic.tiff"),
+  overwrite = TRUE,
+  wopt = wopt
+)
+gc()
 
+# reload
+r <- terra::rast(here("data", "WORLDCOVER", "1_ESA_WC_mosaic.tiff"))
+
+# get AOI object with matching CRS
+aoi <- terra::project(extent, terra::crs(r))
+
+# crop
+terra::crop(
+  r,
+  aoi,
+  filename = here("data", "WORLDCOVER", "2_ESA_WC_mosaic_cropped.tiff"))
+
+# mask
+r <- terra::rast(here("data", "WORLDCOVER", "2_ESA_WC_mosaic_cropped.tiff"))
+terra::mask(
+  r,
+  aoi,
+  filename = here("data", "WORLDCOVER", "3_ESA_WC_mosaic_cropped_masked.tiff")
+  )
+
+# reproject to original extent CRS (3035)
+r <- terra::rast(here("data", "WORLDCOVER", "3_ESA_WC_mosaic_cropped_masked.tiff"))
+terra::project(
+  r,
+  y = terra::crs(extent),
+  filename = here("data", "WORLDCOVER", "ESA_WC_3035.tiff")
+  )
+
+# - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
