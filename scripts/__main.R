4032e5647d388142a52dc4bdd2a517f00e92eb31
zbubster <zbu.rataj@seznam.cz>
2026-02-16 11:41:41 +0100

    added wopt, comments

--- scripts/__main.R
+++ scripts/__main.R
@@ -6,7 +6,6 @@ library(here)
 source(here("scripts", "knihovnik.R"))
 knihovnik(terra, sf, dplyr, stars, ggplot2, tidyr, openeo, biomod2, paisaje, stringr, parallelly)
 
-
 #RColorBrewer
 #viridis
 
--- scripts/esa_wc.R
+++ scripts/esa_wc.R
@@ -2,14 +2,25 @@
 # ESA world cover
 # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
 
+# This script loads TERRASCOPE WORLDCOVER data tiles by ESA (downloaded elsewhere,
+# for example on the internet: https://viewer.esa-worldcover.org/worldcover/), than
+# merge them into one global mosaic, crop them according to study extent, mask outter
+# values as NA and reproject the final product to desired CRS (this is delivered from
+# polygon representing AOI ‒ extent obj).
 # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
 
 # Load data
 
 # dirs & files
 esa_dir <- here("data", "WORLDCOVER")
-extent <- vect(here("data", "extent_raw.gpkg"))
+extent <- vect(here("data", "extent_raw.gpkg")) # AOI, CRS for final output will be taken from this
+# GDAL write options
+wopt <- list(
+  datatype = "FLT4S",
+  gdal = c("COMPRESS=LZW", "TILED=YES", "BIGTIFF=YES")
+)
 
+# list files
 files <- list.files(
   path = esa_dir,
   pattern = "Map\\.tif$",
@@ -19,12 +30,6 @@ files
 
 # load SpatRasterCollection
 rc <- terra::sprc(files)
-
-# GDAL write options
-wopt <- list(
-  datatype = "FLT4S",
-  gdal = c("COMPRESS=LZW", "TILED=YES", "BIGTIFF=YES")
-)
 
 # Mosaic
 mosaic(
@@ -46,14 +51,17 @@ aoi <- terra::project(extent, terra::crs(r))
 terra::crop(
   r,
   aoi,
-  filename = here("data", "WORLDCOVER", "2_ESA_WC_mosaic_cropped.tiff"))
+  filename = here("data", "WORLDCOVER", "2_ESA_WC_mosaic_cropped.tiff"),
+  wopt = wopt
+  )
 
 # mask
 r <- terra::rast(here("data", "WORLDCOVER", "2_ESA_WC_mosaic_cropped.tiff"))
 terra::mask(
   r,
   aoi,
-  filename = here("data", "WORLDCOVER", "3_ESA_WC_mosaic_cropped_masked.tiff")
+  filename = here("data", "WORLDCOVER", "3_ESA_WC_mosaic_cropped_masked.tiff"),
+  wopt = wopt
   )
 
 # reproject to original extent CRS (3035)
@@ -61,7 +69,8 @@ r <- terra::rast(here("data", "WORLDCOVER", "3_ESA_WC_mosaic_cropped_masked.tiff
 terra::project(
   r,
   y = terra::crs(extent),
-  filename = here("data", "WORLDCOVER", "ESA_WC_3035.tiff")
+  filename = here("data", "WORLDCOVER", "ESA_WC_3035.tiff"),
+  wopt = wopt
   )
 
 # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
