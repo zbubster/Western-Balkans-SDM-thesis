0eccbeaf403720ef663c22b8488bc4c55cf63a9b
zbubster <zbu.rataj@seznam.cz>
2026-02-22 18:42:08 +0100

    recomputing CHELSA

--- scripts/for_back_2.R
+++ scripts/for_back_2.R
@@ -12,40 +12,32 @@ ref_1000 <- terra::rast(here("data", "__COMPATIBILITY__", "ref_1000.tif"))
 
 source(here("scripts", "fun_reproject_tree_to_ref.R"))
 
-reproject_tree_to_ref(
-  root_in = here("data", "__COMPATIBILITY__", "CHELSA", "CHELSA_1000"),
-  root_out = here("data", "__COMPATIBILITY__", "CHELSA", "CHELSA_500"),
-  reference = ref_500,
-  method = "near",
-  tol = 1e-7
-)
-
-# splitting into smaller grid 200
-# ORIGINAL DATA: CHELSA_1000
-reproject_tree_to_ref(
-  root_in = here("data", "__COMPATIBILITY__", "CHELSA", "CHELSA_1000"),
-  root_out = here("data", "__COMPATIBILITY__", "CHELSA", "CHELSA_200"),
-  reference = ref_200,
-  method = "near",
-  tol = 1e-7
-)
+# # splitting into smaller grid 200
+# # ORIGINAL DATA: CHELSA_1000
+# reproject_tree_to_ref(
+#   root_in = here("data", "__COMPATIBILITY__", "CHELSA", "CHELSA_1000"),
+#   root_out = here("data", "__COMPATIBILITY__", "CHELSA", "CHELSA_200"),
+#   reference = ref_200,
+#   method = "near",
+#   tol = 1e-7
+# )
 
 # splitting into smaller grid 100
 # ORIGINAL DATA: CHELSA_1000
-reproject_tree_to_ref(
-  root_in = here("data", "__COMPATIBILITY__", "CHELSA", "CHELSA_1000"),
-  root_out = here("data", "__COMPATIBILITY__", "CHELSA", "CHELSA_100"),
-  reference = ref_100,
-  method = "near",
-  tol = 1e-7
-)
+# reproject_tree_to_ref(
+#   root_in = here("data", "__COMPATIBILITY__", "CHELSA", "CHELSA_1000", "CHELSA_v21"),
+#   root_out = here("data", "__COMPATIBILITY__", "CHELSA", "CHELSA_100"),
+#   reference = ref_100,
+#   method = "near",
+#   tol = 1e-7
+# )
 
-# splitting into smaller grid 20
-# ORIGINAL DATA: CHELSA_1000
-reproject_tree_to_ref(
-  root_in = here("data", "__COMPATIBILITY__", "CHELSA", "CHELSA_1000"),
-  root_out = here("data", "__COMPATIBILITY__", "CHELSA", "CHELSA_20"),
-  reference = ref_20,
-  method = "near",
-  tol = 1e-7
-)
+# # splitting into smaller grid 20
+# # ORIGINAL DATA: CHELSA_1000
+# reproject_tree_to_ref(
+#   root_in = here("data", "__COMPATIBILITY__", "CHELSA", "CHELSA_1000", "CHELSA_v21"),
+#   root_out = here("data", "__COMPATIBILITY__", "CHELSA", "CHELSA_20"),
+#   reference = ref_20,
+#   method = "near",
+#   tol = 1e-7
+# )
