142fc0a910a9109eb8a4a69e9da0b52fd6d00ee3
zbubster <zbu.rataj@seznam.cz>
2026-07-03 23:47:01 +0200

    shape finalized

--- scripts/ANA_Shape_parallel_implemenation.R
+++ scripts/ANA_Shape_parallel_implemenation.R
@@ -11,10 +11,11 @@ source(fun_file)
 # main config
 grains <- c(1000, 500, 200, 100)
 species <- c("GD", "GT", "SB", "PK", "PO", "PP")
-shape_id <- "recent_extrapol_weights_all_selected"
+
+shape_id <- "recent_noextrapol_weights_common"
 occ_base_dir <- here::here("data", "__ANALYSIS__", "OCC", "weights")
-pred_base_dir <- here::here("data", "__PREDICTORS_STACKS__", "recent", "selected_predictors_stacks", "extrapol")
-collinearity_type <- "_all_selected"
+pred_base_dir <- here::here("data", "__PREDICTORS_STACKS__", "recent", "selected_predictors_stacks", "noextrapol")
+collinearity_type <- "_common"
 
 # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
 
